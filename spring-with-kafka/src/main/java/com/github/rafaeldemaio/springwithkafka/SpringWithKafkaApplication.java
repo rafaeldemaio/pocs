59728d8585a7c449fa6f9b545ba28317a9ff284d
Rafael Freire <rafael.freire@MacBook-Pro-Rafael-Freire.local>
2020-03-17 15:45:02 -0300

    Initial commit to poc with spring + kafka

---
+++ spring-with-kafka/src/main/java/com/github/rafaeldemaio/springwithkafka/SpringWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.rafaeldemaio.springwithkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithKafkaApplication.class, args);
+	}
+
+}
---
+++ spring-with-kafka/src/test/java/com/github/rafaeldemaio/springwithkafka/SpringWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rafaeldemaio.springwithkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
